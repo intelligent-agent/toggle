62a827ab2a0eacdb185e642e50c5d345203e14fe
Elias <elias@iagent.no>
2014-01-12 21:00:39 +0100

    Working proof-of-the-pudding versjon with touch interface. This version matches the video posted on youtube.

--- c/plate_actor.c
+++ c/plate_actor.c
@@ -1,12 +1,14 @@
 #include "plate_actor.h"
 
 #include <cogl/cogl.h>
+#include <string.h>
 
 G_DEFINE_TYPE (ClutterPlate, clutter_plate, CLUTTER_TYPE_ACTOR);
 
 enum
 {
-  PROP_0
+  PROP_0, 
+  PROP_MATRIX
 };
 
 static CoglVertexP3T2 vertices[] =
@@ -39,6 +41,21 @@ static CoglVertexP3T2 vertices[] =
   { /* pos = */ -1.0f, -1.0f,  1.0f, /* tex coords = */ 1.0f, 0.0f},
   { /* pos = */ -1.0f, -1.0f, -1.0f, /* tex coords = */ 1.0f, 1.0f},
 
+  /* Bottom grid */
+  { /* pos = */  -1.0f, -1.0f, -0.5f, /* tex coords = */ 0.0f, 1.0f},
+  { /* pos = */   1.0f, -1.0f, -0.5f, /* tex coords = */ 0.0f, 1.0f},
+  { /* pos = */  -1.0f, -1.0f,  0.0f, /* tex coords = */ 0.0f, 1.0f},
+  { /* pos = */   1.0f, -1.0f,  0.0f, /* tex coords = */ 0.0f, 1.0f},
+  { /* pos = */  -1.0f, -1.0f,  0.5f, /* tex coords = */ 0.0f, 1.0f},
+  { /* pos = */   1.0f, -1.0f,  0.5f, /* tex coords = */ 0.0f, 1.0f},
+
+  { /* pos = */   0.5f, -1.0f, -1.0f, /* tex coords = */ 0.0f, 1.0f},
+  { /* pos = */   0.5f, -1.0f,  1.0f, /* tex coords = */ 0.0f, 0.0f},
+  { /* pos = */   0.0f, -1.0f, -1.0f, /* tex coords = */ 0.0f, 1.0f},
+  { /* pos = */   0.0f, -1.0f,  1.0f, /* tex coords = */ 0.0f, 0.0f},
+  { /* pos = */  -0.5f, -1.0f, -1.0f, /* tex coords = */ 0.0f, 1.0f},
+  { /* pos = */  -0.5f, -1.0f,  1.0f, /* tex coords = */ 0.0f, 0.0f},
+
   /* Right face */
   { /* pos = */ 1.0f, -1.0f, -1.0f, /* tex coords = */ 1.0f, 0.0f},
   { /* pos = */ 1.0f,  1.0f, -1.0f, /* tex coords = */ 1.0f, 1.0f},
@@ -64,55 +81,76 @@ struct _ClutterPlatePrivate{
     CoglPrimitive   *prim;    
     CoglTexture     *texture;
 
+    CoglMatrix matrix;
     int width; 
     int height; 
-    
+
 };
 
 static void
-do_plate_paint (ClutterActor *self){
+my_actor_paint_node (ClutterActor     *actor,
+                     ClutterPaintNode *root){
+    ClutterPaintNode    *node;
+    ClutterActorBox     box;
     ClutterPlate        *plate;
     ClutterPlatePrivate *priv;
-    CoglColor           *color = cogl_color_new();
-    gfloat texcoords[4] = { 0, 0, 1, 1 };
-    CoglHandle material;
+    CoglMatrix                  transform;
 
-    plate = CLUTTER_PLATE(self);
+    CoglFramebuffer     *screen; 
+    screen = cogl_get_draw_framebuffer ();
+    
+    plate = CLUTTER_PLATE(actor);
     priv = plate->priv;
 
-    /* Paint the plate with the actor's color: */
-    cogl_color_init_from_4f(color, 1.0f, 1.0f, 1.0f, 1.0f);
+    node = clutter_color_node_new(CLUTTER_COLOR_White);
 
-    // Clear the framebuffer
-    cogl_framebuffer_clear4f (priv->fb,
-        COGL_BUFFER_BIT_COLOR|COGL_BUFFER_BIT_DEPTH,
-        0, 0, 0, 1);
+    clutter_paint_node_add_primitive(node, priv->prim);
 
-    // Paint the primitive in the offscreen framebuffer
-    cogl_framebuffer_push_matrix (priv->fb);
-    cogl_pipeline_set_color (priv->pipeline, color);
-    cogl_framebuffer_translate(priv->fb,  priv->width/2, priv->height/2, 0);
-    cogl_framebuffer_scale (priv->fb, 100, 100, 100);
-    cogl_primitive_draw (priv->prim, priv->fb, priv->pipeline);
-    cogl_framebuffer_pop_matrix (priv->fb);    
-
-    
-    // Draw the frambuffer as the actor. TODO: change this with framebuffer cmds. 
-    cogl_set_source_texture (priv->texture);
-    cogl_rectangle_with_texture_coords (0, 0,
-                                        priv->width, priv->height,
-                                        0,0,1,1);
+    /* add the node, and transfer ownership */
+    clutter_paint_node_add_child (root, node);
+    clutter_paint_node_unref (node);
 }
 
 static void
-clutter_plate_paint (ClutterActor *self){
-  do_plate_paint (self);
+clutter_plate_set_property (GObject      *object,
+				guint         prop_id,
+				const GValue *value,
+				GParamSpec   *pspec)
+{
+  ClutterPlate *plate = CLUTTER_PLATE(object);
+
+  switch (prop_id)
+    {
+    case PROP_MATRIX:
+      clutter_plate_set_matrix (plate, g_value_get_boxed (value));
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
 }
 
 static void
-clutter_plate_pick (ClutterActor *self, const ClutterColor *color){
-  do_plate_paint (self);
+clutter_plate_get_property (GObject    *object,
+				guint       prop_id,
+				GValue     *value,
+				GParamSpec *pspec)
+{
+  ClutterPlate *plate = CLUTTER_PLATE(object);
+  ClutterMatrix     matrix;
+
+  switch (prop_id)
+    {
+    case PROP_MATRIX:
+      clutter_plate_get_matrix (plate, &matrix);
+      g_value_set_boxed (value, &matrix);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
 }
+
 
 static void
 clutter_plate_finalize (GObject *object)
@@ -134,11 +172,25 @@ clutter_plate_class_init (ClutterPlateClass *klass)
   ClutterActorClass *actor_class = CLUTTER_ACTOR_CLASS (klass);
 
   /* Provide implementations for ClutterActor vfuncs: */
-  actor_class->paint = clutter_plate_paint;
-  actor_class->pick = clutter_plate_pick;
+  actor_class->paint_node = my_actor_paint_node;
 
   gobject_class->finalize     = clutter_plate_finalize;
   gobject_class->dispose      = clutter_plate_dispose;
+  gobject_class->set_property = clutter_plate_set_property;
+  gobject_class->get_property = clutter_plate_get_property;
+
+  /**
+   * ClutterPlate:cogl-matrix:
+   *
+   * The CoglMatrix of the plate.
+   */
+  g_object_class_install_property (gobject_class,
+                                   PROP_MATRIX,
+                                   g_param_spec_boxed ("matrix",
+                                                       "Matrix",
+                                                       "The CoglMatrix of the plate",
+                                                       CLUTTER_TYPE_MATRIX,
+                                                       G_PARAM_READABLE | G_PARAM_WRITABLE));
 
   g_type_class_add_private (gobject_class, sizeof (ClutterPlatePrivate));
 }
@@ -147,7 +199,7 @@ static void
 clutter_plate_init (ClutterPlate *self){
     ClutterPlatePrivate *priv;
     ClutterBackend      *be         = clutter_get_default_backend ();
-    CoglContext         *ctx        = clutter_backend_get_cogl_context (be);
+    CoglContext         *ctx        = (CoglContext*) clutter_backend_get_cogl_context (be);
     CoglOffscreen       *offscreen;
     CoglMatrix          view;
     int width, height;
@@ -155,7 +207,6 @@ clutter_plate_init (ClutterPlate *self){
 
 
     self->priv       = priv = CLUTTER_PLATE_GET_PRIVATE (self);
-    //priv->fb         = cogl_get_draw_framebuffer ();
     priv->pipeline   = cogl_pipeline_new(ctx);
 
     priv->width = width = 400;
@@ -165,36 +216,40 @@ clutter_plate_init (ClutterPlate *self){
     priv->prim = cogl_primitive_new_p3t2 (ctx, COGL_VERTICES_MODE_LINES,
                                        G_N_ELEMENTS (vertices),
                                        vertices);
-
-    
     // Make a new blank texture
-    priv->texture = cogl_texture_new_with_size (width, height,
-                                                 COGL_TEXTURE_NONE,
-                                                 COGL_PIXEL_FORMAT_RGB_888);
+    //priv->texture = cogl_texture_new_with_size (width, height,
+    //                                             COGL_TEXTURE_NONE,
+    //                                             COGL_PIXEL_FORMAT_RGB_888);
 
     // Init the Offscreen buffer from the texture
-    offscreen = cogl_offscreen_new_with_texture (priv->texture);
-    priv->fb = COGL_FRAMEBUFFER (offscreen);
+    //offscreen = cogl_offscreen_new_with_texture (priv->texture);
+    //priv->fb = COGL_FRAMEBUFFER (offscreen);
     
-    cogl_framebuffer_set_viewport (priv->fb,
-                                 0, 0,
-                                 width,
-                                 height);
+    //cogl_framebuffer_set_viewport (priv->fb,
+    //                             0, 0,
+    //                             width,
+    //                             height);
 
-    fovy = 60; /* y-axis field of view */
-    aspect = width/height;
-    z_near = 0.1; /* distance to near clipping plane */
-    z_2d = 1000; /* position to 2d plane */
-    z_far = 2000; /* distance to far clipping plane */
+    //fovy = 60; /* y-axis field of view */
+    //aspect = width/height;
+    //z_near = 0.1; /* distance to near clipping plane */
+    //z_2d = 1000; /* position to 2d plane */
+    //z_far = 2000; /* distance to far clipping plane */
 
+    /*
     cogl_framebuffer_perspective (priv->fb, fovy, aspect, z_near, z_far);
 
     cogl_matrix_init_identity (&view);
     cogl_matrix_view_2d_in_perspective (&view, fovy, aspect, z_near, z_2d,
                                       width,
                                       height);
+    */
+    //cogl_framebuffer_set_modelview_matrix (priv->fb, &view);
 
-    cogl_framebuffer_set_modelview_matrix (priv->fb, &view);
+    //cogl_matrix_init_identity (&priv->matrix); 
+    //cogl_matrix_translate(&priv->matrix,  width/2, height/2, 0);
+    //cogl_matrix_scale (&priv->matrix, 100, 100, 100);
+    //cogl_matrix_rotate(&priv->matrix, 45, 0, 1, 0);
 }
 
 /**
@@ -209,3 +264,51 @@ clutter_plate_new (void){
   return g_object_new (CLUTTER_TYPE_PLATE, NULL);
 }
 
+
+/**
+ * clutter_plate_get_matrix:
+ * @plate: a #ClutterPlate
+ * @matrix: return location for a #ClutterMatrix
+ *
+ * Retrieves the matrix of @plate.
+ */
+void 
+clutter_plate_get_matrix (ClutterPlate *plate, 
+                          const ClutterMatrix *matrix){
+    ClutterPlatePrivate *priv;
+
+    g_return_if_fail (CLUTTER_IS_PLATE (plate));
+    g_return_if_fail (matrix != NULL);
+
+    priv = plate->priv;
+
+    memcpy((void *)matrix, &priv->matrix, sizeof(CoglMatrix));
+}
+
+/**
+ * clutter_plate_set_matrix:
+ * @plate: a #ClutterPlate
+ * @matrix: a #CoglMatrix
+ *
+ * Sets the matrix of @plate.
+ */
+void
+clutter_plate_set_matrix (ClutterPlate   *plate,
+			     const ClutterMatrix *matrix){
+    ClutterPlatePrivate *priv;
+
+    g_return_if_fail (CLUTTER_IS_PLATE (plate));
+    g_return_if_fail (matrix != NULL);
+
+    g_object_ref (plate);
+
+    priv = plate->priv;
+
+    memcpy(&priv->matrix, matrix, sizeof(CoglMatrix));
+
+    if (CLUTTER_ACTOR_IS_VISIBLE (CLUTTER_ACTOR (plate)))
+        clutter_actor_queue_redraw (CLUTTER_ACTOR (plate));
+
+    g_object_notify (G_OBJECT (plate), "matrix");
+    g_object_unref (plate);
+}
--- c/toggle.c
+++ c/toggle.c
@@ -3,20 +3,66 @@
 #include <mash/mash.h>
 #include <stdlib.h>
 #include "plate_actor.h"
+#include "clutter-volume-stage.h"
 
-ClutterActor *model;
-gdouble rotation = 0;
+ClutterActor *model, *plate, *volume_stage, *volume_viewport;
+ClutterMatrix mat;
+float rotation;
+gboolean touching = FALSE;
+gfloat port_x, port_y;
+gfloat start_x, start_y;
+gfloat current_x, current_y;
+gfloat delta_x, delta_y;
 
 void on_timeline_new_frame(ClutterTimeline *timeline, gint frame_num, gpointer data) {
+    if(!touching){
         rotation += 0.3;
-
-        clutter_actor_set_rotation_angle(model, CLUTTER_Y_AXIS, rotation );
+        clutter_actor_set_rotation_angle(volume_stage, CLUTTER_Y_AXIS, rotation );
+    }
 }
 
+void
+on_enter_volume_viewport(ClutterActor *actor,
+               ClutterEvent *event){
+    fprintf(stderr, "enter-viewport\n");
+    touching = TRUE;
+    clutter_event_get_coords(event, &start_x, &start_y);
+}
+void
+on_leave_stage(ClutterActor *actor,
+               ClutterEvent *event){
+    fprintf(stderr, "leave-stage\n");
+    if(touching){
+        touching = FALSE;
+    }
+}
+void
+on_motion_event(ClutterActor *actor,
+                ClutterEvent *event){
+    clutter_event_get_coords(event, &port_x, &port_y);
+    fprintf(stderr, "motion-stage %f, %f\n", port_x, port_y);
+    if(touching){
+        delta_x = (port_x-start_x);
+        delta_y = (port_y-start_y);
+        clutter_actor_set_rotation_angle(volume_stage, CLUTTER_X_AXIS, current_x+delta_x/10.0 );
+        clutter_actor_set_rotation_angle(volume_stage, CLUTTER_Y_AXIS, current_y+delta_y/10.0 );
+        current_x = delta_x;
+        current_y = delta_y;
+    }
+}
+
+void
+on_motion_viewport(ClutterActor *actor, 
+                ClutterEvent *event){
+    fprintf(stderr, "motion-viewport\n");
+}
+
+
+// Main
 int main(int argc, char *argv[]) {
-        int ret;
+    int ret;
 	GError* err = NULL;
-	ClutterActor *stage, *plate, *rect;
+	ClutterActor *stage;
 	ClutterActor *box;
 	MashLightSet *light_set;
 	ClutterActor *light_point, *light_directional, *light_spot;
@@ -24,40 +70,48 @@ int main(int argc, char *argv[]) {
 
     ret = clutter_init(&argc, &argv);
 
-    ClutterActor* plate2 = clutter_plate_new();
+    // Make a transformation matrix and apply it to the plate
+    cogl_matrix_init_identity(&mat);    
+    cogl_matrix_translate(&mat,  200, 200, 0);
+    cogl_matrix_scale (&mat, 100, 100, 100);
+    cogl_matrix_rotate(&mat, 180, 0, 0, 1);
+
+    // Dummy call to make sure it gets loaded
+    clutter_plate_new();
+
+	const gchar *paths[] = { "/usr/src/toggle/etc" };
 
 	style = mx_style_get_default ();
 	if (!mx_style_load_from_file (style, "../etc/style.css", &err)){
       		g_warning ("Error setting style: %s", err->message);
       		g_clear_error (&err);
-    	}
+    }
 
+    // Load the scene using clutterscript
 	gchar *filename = "ui.json";
-	const gchar *paths[] = { "/usr/src/toggle/etc" };
-
 	ClutterScript *ui = clutter_script_new();
 	clutter_script_add_search_paths (ui, paths, 1);
 	clutter_script_load_from_file(ui, "../etc/ui.json", &err);
 
-        clutter_script_get_objects (ui,"stage", &stage, NULL);
-        clutter_script_connect_signals (ui, ui);
-        clutter_actor_show(stage);
+    clutter_script_get_objects (ui,"stage", &stage, NULL);
+    clutter_script_connect_signals (ui, ui);
+    clutter_actor_show(stage);
 
-        clutter_script_get_objects(ui, "3d-stage", &plate, NULL);
+    clutter_script_get_objects(ui, "volume-viewport", &volume_viewport, NULL);
+    clutter_script_get_objects(ui, "volume-stage", &volume_stage, NULL);
+    clutter_script_get_objects(ui, "plate", &plate, NULL);
 
 	if (err != NULL){
 		g_critical ("Error loading ClutterScript file %s\n%s", filename, err->message);
       		g_error_free (err);
       		exit (EXIT_FAILURE);
-    	}
+    }
 
-        ClutterColor actor_color = { 0x00, 0xCC, 0x00, 0xFF };
-
-	if ((model = mash_model_new_from_file (MASH_DATA_NONE, "../models/teapot.ply", &err))
-	== NULL){
+	if ((model = mash_model_new_from_file (MASH_DATA_NONE, 
+                                            "../models/teapot.ply", &err)) == NULL){
       		g_warning ("Failed to load model: %s\n", err->message);
       		g_clear_error (&err);
-    	}
+    }
 
 	// Set up the light
 	light_set = mash_light_set_new ();
@@ -69,20 +123,19 @@ int main(int argc, char *argv[]) {
 	ClutterColor diffuse  = { 128, 128, 128, 128 };
 	ClutterColor specular = { 128, 128, 128, 128 };
 
-        mash_light_set_add_light (light_set, MASH_LIGHT (light_point));
-        mash_light_set_add_light (light_set, MASH_LIGHT (light_directional));
+    mash_light_set_add_light (light_set, MASH_LIGHT (light_point));
+    mash_light_set_add_light (light_set, MASH_LIGHT (light_directional));
 	mash_light_set_add_light (light_set, MASH_LIGHT (light_spot));
  	mash_model_set_light_set (MASH_MODEL (model), light_set);
 	g_object_unref (light_set);
 
-	// Add the model and the light
-	clutter_actor_add_child(plate, light_point);
-	clutter_actor_add_child(plate, light_directional);
-	clutter_actor_add_child(plate, light_spot);
-	clutter_actor_add_child(plate, model);
+	// Add the model the lights to the volume viewport
+	clutter_actor_add_child(volume_viewport, light_point);
+	clutter_actor_add_child(volume_viewport, light_directional);
+	clutter_actor_add_child(volume_viewport, light_spot);
 
 	// Make a new material for the model
-	CoglMaterial *material = cogl_material_new();
+	CoglMaterial *material = (CoglMaterial *) cogl_material_new();
 	CoglColor *model_color = cogl_color_new();
 	cogl_color_init_from_4f(model_color, 0.0, 1.0, 0.0, 1.0);
 
@@ -92,24 +145,37 @@ int main(int argc, char *argv[]) {
 	cogl_material_set_diffuse(material, 0.5, 0.5, 0.5, 0.5);
 	mash_model_set_material (MASH_MODEL (model), material);
 
+
+
 	// Position and size the model
-	clutter_actor_set_pivot_point (model, 0.5, 0.5);
-	clutter_actor_set_position(model, clutter_actor_get_width(plate)/2.0, clutter_actor_get_height(plate)/2.0);
-	clutter_actor_set_rotation_angle(model, CLUTTER_X_AXIS, 180.0);
-	//clutter_actor_set_scale (model, 100.0, 100.0);
+	//clutter_actor_set_pivot_point (model, 0.5, 0.5);
+	//clutter_actor_set_position(model, clutter_actor_get_width(plate)/2.0, clutter_actor_get_height(plate)/2.0);
+	//clutter_actor_set_rotation_angle(model, CLUTTER_X_AXIS, 180.0);
+    clutter_actor_add_child(volume_stage, model);
+
+    
+    clutter_actor_set_pivot_point (volume_stage, 0.5, 0.5);
 
 	// Make the model spin
-        ClutterTimeline *timeline = clutter_timeline_new(60);
-        g_signal_connect(timeline, "new-frame", G_CALLBACK(on_timeline_new_frame), NULL);
-        clutter_timeline_set_repeat_count(timeline, -1);
-        clutter_timeline_start(timeline);
+    ClutterTimeline *timeline = clutter_timeline_new(60);
+    g_signal_connect(timeline, "new-frame", G_CALLBACK(on_timeline_new_frame), NULL);
+    clutter_timeline_set_repeat_count(timeline, -1);
+    clutter_timeline_start(timeline);
 
-	/* Rotate the stage */
-        clutter_script_get_objects(ui, "box", &box, NULL);
-        clutter_actor_set_rotation_angle (box, CLUTTER_Z_AXIS, -90.0f);
-        clutter_actor_set_position(box, 0, 800);
+    //g_signal_connect (stage, "touch-event", G_CALLBACK (on_touch_event), NULL);
+    //g_signal_connect (stage, "event", G_CALLBACK (on_event), NULL);
+    g_signal_connect (stage, "motion-event", G_CALLBACK (on_motion_event), NULL);
+    g_signal_connect (volume_viewport, "motion-event", G_CALLBACK (on_motion_viewport), NULL);
+    g_signal_connect (stage, "enter-event", G_CALLBACK (on_enter_volume_viewport), NULL);
+    g_signal_connect (stage, "leave-event", G_CALLBACK (on_leave_stage), NULL);
+    
 
-        clutter_main();
+	/* Rotate the display/stage */
+    clutter_script_get_objects(ui, "box", &box, NULL);
+    clutter_actor_set_rotation_angle (box, CLUTTER_Z_AXIS, -90.0f);
+    clutter_actor_set_position(box, 0, 800);
 
-        return EXIT_SUCCESS;
+    clutter_main();
+
+    return EXIT_SUCCESS;
 }
