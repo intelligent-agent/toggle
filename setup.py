fc260ee14b99ecd77747b85f5aa8cdfb67e32f48
Elias Bakken <elias@iagent.no>
2014-11-27 18:01:14 +0100

    Preparing Toggle package

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
+
+from setuptools import setup, find_packages, Extension
 import os
-from distutils.core import setup
+#from distutils.core import setup, Extension
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+#toggle_lib = Extension('toggl_lib',
+#                    sources = [
+#                        'toggle-lib/toggle-model.c', 
+#                        'toggle-lib/toggle-box.c'])
 
 setup(name='Toggle',
       version='0.4.0.dev1',
@@ -14,24 +22,34 @@ setup(name='Toggle',
       url='http://www.thing-printer.com',
       license = "CC-BY-SA-2.0",
       long_description=read('README.md'),
-      py_modules=[
-            'Toggle', 
-            'Message', 
-            'MessageListener', 
-            'Plate', 
-            'Model', 
-            'VolumeStage', 
-            'ModelLoader', 
-            'Printer', 
-            'CascadingConfigParser'],
+      packages = find_packages(),
       classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Program",
         "License :: OSI Approved :: CC-BY-NC-2.0 License"],
       data_files=[('/etc/toggle/platforms', ['models/platforms/prusa.stl']),
                   ('/usr/share/models/', ['models/calibration-cube.stl', 'models/treefrog.stl']),
-                  ('/etc/toggle/style', ['style/style.css', 'style/ui.json']),
-                  ('/etc/toggle', ['configs/default.cfg'])],
-      scripts=['toggle'], 
+                  ('/etc/toggle/style', ['style/style.css', 'style/ui.json', 'style/button.png', 'style/button-checked.png']),
+                  ('/etc/toggle', ['configs/default.cfg']),
+                  ('/lib/systemd/system/', ['systemd/toggle.service', 'systemd/toggle.path']), 
+        		  ('./', ['README.md'])],
+      entry_points = {
+            'console_scripts': [
+                'toggle = toggle.Toggle:main'
+            ]
+        },
+      #ext_modules = [toggle_lib]
      )
 
+'''
+      py_modules=[
+            'toggle/Toggle', 
+            'toggle/Message', 
+            'toggle/MessageListener', 
+            'toggle/Plate', 
+            'toggle/Model', 
+            'toggle/VolumeStage', 
+            'toggle/ModelLoader', 
+            'toggle/Printer', 
+            'toggle/CascadingConfigParser'],
+'''
