b7d45b58fe346ebb9ca188b9de717cfc4506736a
Elias Bakken <elias@iagent.no>
2016-04-06 14:35:56 +0200

    Added jog distance change as pr Issue #16

--- README.md
+++ README.md
@@ -303,3 +303,29 @@ make install
 
 
 CLUTTER_BACKEND=eglnative toggle
+
+
+
+##Debian Stretch install instructions:
+
+
+Standard packages in Stretch:
+libinput-1.2.2-1
+Cogl-1.22.0-2
+Clutter-1.26.0-2
+libmx-1.0-2 (1.4.7-1 and others)
+
+**Mash (with STL-import)**
+Note: Disable compiling the examples and 
+use the right --library format for the g-ir-scanner
+```
+cd /usr/src/
+git clone https://github.com/eliasbakken/mash.git
+cd /usr/src/mash
+./autogen.sh --prefix=/usr --libdir=/usr/lib/arm-linux-gnueabihf/ --enable-introspection
+sed -i 's/SUBDIRS = mash examples docs/SUBDIRS = mash docs/' Makefile
+sed -i 's/--library=libmash-@MASH_API_VERSION@.la/--library=mash-@MASH_API_VERSION@/ --library-path=/usr/src/mash/mash/.libs/' mash/Makefile.am
+make
+make install
+```
+
--- toggle/Jog.py
+++ toggle/Jog.py
@@ -6,8 +6,11 @@ import logging
 class Jog:
     def __init__(self, config=None):
         self.config = config
-        self.jog_amount = 10 # 10 mm default jog amount
-        self.extrude_amount = 10 # 10 mm deafult extrude amount
+        self.jog_xy_amount = 10 # 10 mm default jog amount
+        self.jog_z_amount = 10       
+        self.jog_eh_amount = 10 # 10 mm deafult extrude amount
+
+        self.amount_list = [0.1, 1, 10, 100]
 
         buttons = [
             "jog_x_minus", 
@@ -20,7 +23,10 @@ class Jog:
             "jog_z_home", 
             "jog_e_extrude", 
             "jog_e_retract", 
-            "jog_e_toggle"
+            "jog_e_toggle", 
+            "travel_xy",
+            "travel_z",
+            "travel_eh"
         ]
 
         for name in buttons:
@@ -35,34 +41,34 @@ class Jog:
 
     
     def jog_x_minus(self, btn, etc=None, other=None):
-        self.config.rest_client.jog({"x": -self.jog_amount})
+        self.config.rest_client.jog({"x": -self.jog_xy_amount})
 
     def jog_x_plus(self, btn, etc=None, other=None):
-        self.config.rest_client.jog({"x": self.jog_amount})
+        self.config.rest_client.jog({"x": self.jog_xy_amount})
 
     def jog_y_minus(self, btn, etc=None, other=None):
-        self.config.rest_client.jog({"y": -self.jog_amount})
+        self.config.rest_client.jog({"y": -self.jog_xy_amount})
      
     def jog_y_plus(self, btn, etc=None, other=None):
-        self.config.rest_client.jog({"y": self.jog_amount})
+        self.config.rest_client.jog({"y": self.jog_xy_amount})
 
     def jog_home(self, btn, etc=None, other=None):
         self.config.rest_client.home(["x", "y"])
 
     def jog_z_plus(self, btn, etc=None, other=None):
-        self.config.rest_client.jog({"z": self.jog_amount})
+        self.config.rest_client.jog({"z": self.jog_z_amount})
 
     def jog_z_minus(self, btn, etc=None, other=None):
-        self.config.rest_client.jog({"z": -self.jog_amount})
+        self.config.rest_client.jog({"z": -self.jog_z_amount})
 
     def jog_z_home(self, btn, etc=None, other=None):
        self.config.rest_client.home(["z"]) 
 
     def jog_e_extrude(self, btn, etc=None, other=None):
-        self.config.rest_client.extrude(self.extrude_amount)
+        self.config.rest_client.extrude(self.jog_eh_amount)
 
     def jog_e_retract(self, btn, etc=None, other=None):
-        self.config.rest_client.extrude(-self.extrude_amount)
+        self.config.rest_client.extrude(-self.jog_eh_amount)
 
     def jog_e_toggle(self, tap, btn=None, other=None):
         if btn.get_toggled():
@@ -70,5 +76,53 @@ class Jog:
         else:
             self.config.rest_client.select_tool("tool1")
 
+    def travel_xy(self, tap, btn=None, other=None):
+        logging.debug("tavel_xy")
+        if self.jog_xy_amount == 0.1:
+            self.jog_xy_amount = 1
+            self.config.ui.get_object("travel_xy").set_style_class("travel_1")
+        elif self.jog_xy_amount == 1:
+            self.jog_xy_amount = 10
+            self.config.ui.get_object("travel_xy").set_style_class("travel_10")
+        elif self.jog_xy_amount == 10:
+            self.jog_xy_amount = 100
+            self.config.ui.get_object("travel_xy").set_style_class("travel_100")
+        elif self.jog_xy_amount == 100:
+            self.jog_xy_amount = 0.1
+            self.config.ui.get_object("travel_xy").set_style_class("travel_01")
+        else:
+            logging.warning("Invaid jog amount for XY: "+str(self.jog_xy_amount))            
+    def travel_z(self, tap, btn=None, other=None):
+        logging.debug("tavel_z")
+        if self.jog_z_amount == 0.1:
+            self.jog_z_amount = 1
+            self.config.ui.get_object("travel_z").set_style_class("travel_1")
+        elif self.jog_z_amount == 1:
+            self.jog_z_amount = 10
+            self.config.ui.get_object("travel_z").set_style_class("travel_10")
+        elif self.jog_z_amount == 10:
+            self.jog_z_amount = 100
+            self.config.ui.get_object("travel_z").set_style_class("travel_100")
+        elif self.jog_z_amount == 100:
+            self.jog_z_amount = 0.1
+            self.config.ui.get_object("travel_z").set_style_class("travel_01")
+        else:
+            logging.warning("Invaid jog amount for Z: "+str(self.jog_z_amount))            
 
+    def travel_eh(self, tap, btn=None, other=None):
+        logging.debug("tavel_eh")
+        if self.jog_eh_amount == 0.1:
+            self.jog_eh_amount = 1
+            self.config.ui.get_object("travel_eh").set_style_class("travel_1")
+        elif self.jog_eh_amount == 1:
+            self.jog_eh_amount = 10
+            self.config.ui.get_object("travel_eh").set_style_class("travel_10")
+        elif self.jog_eh_amount == 10:
+            self.jog_eh_amount = 100
+            self.config.ui.get_object("travel_eh").set_style_class("travel_100")
+        elif self.jog_eh_amount == 100:
+            self.jog_eh_amount = 0.1
+            self.config.ui.get_object("travel_eh").set_style_class("travel_01")
+        else:
+            logging.warning("Invaid jog amount for EH: "+str(self.jog_eh_amount))            
 
