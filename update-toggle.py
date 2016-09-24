2522ecb70ee19e39ee9f21b06f60ef188c390ae5
Elias Bakken <elias@iagent.no>
2016-09-24 02:39:55 +0200

    Updating the update script

--- update-toggle.py
+++ update-toggle.py
@@ -71,6 +71,20 @@ def _python(args, cwd, python_executable, sudo=False):
 	return p.returncode, stdout
 
 
+def _make(folder):
+	command = ["sudo", "make", "-C", folder, "install"]
+	try:
+		p = subprocess.Popen(command, stdout=subprocess.PIPE,
+		                     stderr=subprocess.PIPE)
+	except:
+		return None, None
+
+	stdout = p.communicate()[0].strip()
+	if sys.version >= "3":
+		stdout = stdout.decode()
+
+	return p.returncode, stdout
+
 def update_source(git_executable, folder, target, force=False):
 	print(">>> Running: git diff --shortstat")
 	returncode, stdout = _git(["diff", "--shortstat"], folder, git_executable=git_executable)
@@ -121,20 +135,34 @@ def install_source(python_executable, folder, user=False, sudo=False):
 		print("Continuing anyways")
 	print(stdout)
 
-	print(">>> Running: python setup.py install")
-	args = ["setup.py", "install"]
-	if user:
-		args.append("--user")
-	returncode, stdout = _python(args, folder, python_executable, sudo=sudo)
+	print(">>> Running: make -C /usr/src/toggle install")
+	returncode, stdout = _make("/usr/src/toggle")
 	if returncode != 0:
-		raise RuntimeError("Could not update, \"python setup.py install\" failed with returncode %d: %s" % (returncode, stdout))
+		raise RuntimeError("Could not update, \"make install\" failed with returncode %d: %s" % (returncode, stdout))
 	print(stdout)
 
+
+def restart(service):
+	print(">>> Running: sudo systemctl restart "+service)
+	command = ["sudo", "systemctl", "restart", service]
+	try:
+		p = subprocess.Popen(command, stdout=subprocess.PIPE,
+		                     stderr=subprocess.PIPE)
+	except:
+		print("Error restarting")
+
+	stdout = p.communicate()[0].strip()
+	if sys.version >= "3":
+		stdout = stdout.decode()
+        if p.returncode != 0:
+		print("\"systemctl restart redeem\" failed with returncode %d: %s" % (p.returncode, stdout))
+	print(stdout)
+	print("Restart OK")
 
 def parse_arguments():
 	import argparse
 
-	parser = argparse.ArgumentParser(prog="update-octoprint.py")
+	parser = argparse.ArgumentParser(prog="update-toggle.py")
 
 	parser.add_argument("--git", action="store", type=str, dest="git_executable",
 	                    help="Specify git executable to use")
@@ -175,6 +203,7 @@ def main():
 
 	update_source(git_executable, folder, target, force=args.force)
 	install_source(python_executable, folder, user=args.user, sudo=args.sudo)
+	restart("toggle.service")
 
 if __name__ == "__main__":
 	main()
