e1e0bc94f0e38578f07d98f05befcc23febfdf44
TuxNetrunner <ffrancesconi@gmail.com>
2019-02-23 10:36:06 +0100

    Develop (#17)
    * Added Websocket section to config file
    
    * Changed Auth and message parsing for Octoprint 1.3.10 compatibility
    
    * Fixed temperature display for single extruder machines

--- toggle/WebSocksClient.py
+++ toggle/WebSocksClient.py
@@ -44,28 +44,33 @@ class WebSocksClient():
     self._r1 = str(random.randint(0, 1000))
     self._conn_id = self.random_str(8)
 
-    self.url = host + '/'.join([self._prefix, self._r1, self._conn_id, 'websocket'])
+    self.url = host + '/'.join([self._prefix, 'websocket'])
 
     self.max_reconnects = 10
     self.state = WebSocksClient.CLOSED
-
     self.io_loop = ioloop.IOLoop.current()
+
 
   def connect(self):
     logging.debug("Connecting to " + self.url)
     self.state = WebSocksClient.CONNECTING
-    self.conn = websocket.websocket_connect(
+    self._ws_connection = websocket.websocket_connect(
         self.url, io_loop=self.io_loop, callback=self._connect_callback)
+
+  def authenticate(self, apikey):
+    user = self.config.get("WebSocket", "user")
+    logging.debug("Authenticating with " + user + ":" + apikey)
+    msg = '{ "auth" : "' + str(user) + ':' + str(apikey) + '" }'
+    logging.info("Sending message " + msg)
+    self._ws_connection.write_message(msg)
 
   def send(self, data):
     """
     Send message to the server
       :param str data: message.
     """
-    if not self._ws_connection:
-      raise RuntimeError('Web socket connection is closed.')
-
-    self._ws_connection.write_message(escape.utf8(json.dumps(data)))
+    logging.info("Sending message " + data)
+    yield self._ws_connection.write_message(data)
 
   def close_conn(self):
     """
@@ -100,30 +105,20 @@ class WebSocksClient():
     This is called when new message is available from the server.
       :param str msg: server message.
     """
-    #print(msg)
-    data = msg[0]
-    payload = msg[1:]
-    if data == 'o':
+    data = json.loads(msg)
+    if 'connected' in data:
       logging.debug("SockJS: Socket connected")
-    elif data == 'c':
-      logging.debug("SockJS: Socket disconnected, reason: " + str(payload))
-    elif data == 'h':
-      self.config.printer.flash_heartbeat()
-      #logging.debug("SockJS: Heartbeat "+str(msg))
-    elif data in ['a', 'm']:
-      if self.config:
-        self.parse_msg(payload)
-      else:
-        logging.debug(payload)
-    else:
-      logging.debug("Got garbled char " + str(msg) + " hex: " + str(int(msg, 16)))
+      apik = data['connected']['apikey']
+      self.authenticate(apik)
+
+    self.parse_msg(data)
 
   def _on_connection_success(self):
     """
     This is called on successful connection ot the server.
     """
     self.state = WebSocksClient.CONNECTED
-    logging.debug('Webscket connected!')
+    logging.debug('Websocket connected!')
 
   def _on_connection_close(self):
     """
@@ -141,9 +136,8 @@ class WebSocksClient():
     self.io_loop.stop()
     logging.debug('Websocket connection error: %s', exception)
 
-  def parse_msg(self, msg):
+  def parse_msg(self, data):
     try:
-      data = json.loads(msg)[0]
       msg_type, payload = data.popitem()
       p = PushUpdate(msg_type, payload)
       self.config.push_updates.put(p)
@@ -183,8 +177,7 @@ def main():
       format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
       datefmt='%m-%d %H:%M')
 
-  #client = WebSocksClient(None, "ws://kamikaze.local")
-  client = WebSocksClient(None, "ws://localhost")
+  client = WebSocksClient(None, "ws://kamikaze.local")
 
   def work():
     for i in range(10):
