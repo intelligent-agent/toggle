4f2611b855f248fa5fb30204ff14f2f0f37dd28a
Ben Glick <glick@lclark.edu>
2019-03-07 17:05:18 -0500

    Getting python 3 to work on the VM

--- toggle/TemperatureGraph.py
+++ toggle/TemperatureGraph.py
@@ -6,190 +6,189 @@ import logging
 
 
 class TemperatureGraph():
-  def __init__(self, config):
-    self.config = config
+    def __init__(self, config):
+        self.config = config
 
-    # Set up temperature graph
-    self.temp = config.ui.get_object("graph")
-    self.graph = Graph(self.temp.get_width(), self.temp.get_height())
-    self.temp.add_child(self.graph)
-    self.temp.set_reactive(True)
+        # Set up temperature graph
+        self.temp = config.ui.get_object("graph")
+        self.graph = Graph(self.temp.get_width(), self.temp.get_height())
+        self.temp.add_child(self.graph)
+        self.temp.set_reactive(True)
 
-    tap = Clutter.TapAction()
-    self.temp.add_action(tap)
-    tap.connect("tap", self.change_to_filament)
+        tap = Clutter.TapAction()
+        self.temp.add_action(tap)
+        tap.connect("tap", self.change_to_filament)
 
-    self.temps = {
-        "tool0": {
-            "g_actual": GraphPlot("E", (1, 0, 0)),
-            "g_target": GraphPlot("T_E", (1, 0.4, 0.4)),
-            "t_actual": 0,
-            "t_target": 0,
-            "t_preheat": 220,
-            "heating": False,
-            "state": "cold",
-            "btn-name": "heat-tool0",
-            "func-name": self.on_preheat_tool0,
-            "btn": None
-        },
-        "tool1": {
-            "g_actual": GraphPlot("H", (1, 0.64, 0)),
-            "g_target": GraphPlot("T_H", (1, 0.8, 0.5)),
-            "t_actual": 0,
-            "t_target": 0,
-            "t_preheat": 220,
-            "heating": False,
-            "state": "cold",
-            "btn-name": "heat-tool1",
-            "func-name": self.on_preheat_tool1,
-            "btn": None
-        },
-        "tool2": {
-            "g_actual": GraphPlot("A", (1, 0.64, 0)),
-            "g_target": GraphPlot("T_A", (1, 0.8, 0.5)),
-            "t_actual": 0,
-            "t_target": 0,
-            "t_preheat": 220,
-            "heating": False,
-            "state": "cold",
-            "btn-name": "heat-tool2",
-            "func-name": self.on_preheat_tool2,
-            "btn": None
-        },
-        "bed": {
-            "g_actual": GraphPlot("BED", (0, 0, 1)),
-            "g_target": GraphPlot("T_BED", (0.2, 0.8, 0.8)),
-            "t_actual": 0,
-            "t_target": 0,
-            "t_preheat": 65,
-            "heating": False,
-            "state": "cold",
-            "btn-name": "heat-bed",
-            "func-name": self.on_preheat_bed,
-            "btn": None
+        self.temps = {
+            "tool0": {
+                "g_actual": GraphPlot("E", (1, 0, 0)),
+                "g_target": GraphPlot("T_E", (1, 0.4, 0.4)),
+                "t_actual": 0,
+                "t_target": 0,
+                "t_preheat": 220,
+                "heating": False,
+                "state": "cold",
+                "btn-name": "heat-tool0",
+                "func-name": self.on_preheat_tool0,
+                "btn": None
+            },
+            "tool1": {
+                "g_actual": GraphPlot("H", (1, 0.64, 0)),
+                "g_target": GraphPlot("T_H", (1, 0.8, 0.5)),
+                "t_actual": 0,
+                "t_target": 0,
+                "t_preheat": 220,
+                "heating": False,
+                "state": "cold",
+                "btn-name": "heat-tool1",
+                "func-name": self.on_preheat_tool1,
+                "btn": None
+            },
+            "tool2": {
+                "g_actual": GraphPlot("A", (1, 0.64, 0)),
+                "g_target": GraphPlot("T_A", (1, 0.8, 0.5)),
+                "t_actual": 0,
+                "t_target": 0,
+                "t_preheat": 220,
+                "heating": False,
+                "state": "cold",
+                "btn-name": "heat-tool2",
+                "func-name": self.on_preheat_tool2,
+                "btn": None
+            },
+            "bed": {
+                "g_actual": GraphPlot("BED", (0, 0, 1)),
+                "g_target": GraphPlot("T_BED", (0.2, 0.8, 0.8)),
+                "t_actual": 0,
+                "t_target": 0,
+                "t_preheat": 65,
+                "heating": False,
+                "state": "cold",
+                "btn-name": "heat-bed",
+                "func-name": self.on_preheat_bed,
+                "btn": None
+            }
         }
-    }
 
-    for tool in self.temps:
-      # Add graphs
-      self.graph.add_plot(self.temps[tool]["g_actual"])
-      self.graph.add_plot(self.temps[tool]["g_target"])
+        for tool in self.temps:
+            # Add graphs
+            self.graph.add_plot(self.temps[tool]["g_actual"])
+            self.graph.add_plot(self.temps[tool]["g_target"])
 
-      # Set up buttons
-      btn = self.config.ui.get_object(self.temps[tool]["btn-name"])
-      tap = Clutter.TapAction()
-      btn.add_action(tap)
-      tap.connect("tap", self.temps[tool]["func-name"])
-      self.temps[tool]["btn"] = btn
+            # Set up buttons
+            btn = self.config.ui.get_object(self.temps[tool]["btn-name"])
+            tap = Clutter.TapAction()
+            btn.add_action(tap)
+            tap.connect("tap", self.temps[tool]["func-name"])
+            self.temps[tool]["btn"] = btn
 
-    # Add a scale to the plot
-    scale = GraphScale(0, 320, [0, 50, 100, 150, 200, 250, 300])
-    scale.set_title("Temperature")
-    self.graph.add_plot(scale)
+        # Add a scale to the plot
+        scale = GraphScale(0, 320, [0, 50, 100, 150, 200, 250, 300])
+        scale.set_title("Temperature")
+        self.graph.add_plot(scale)
 
-    # set up temp label
-    self.lbl_temp = self.config.ui.get_object("lbl-temp")
-    self.ok_range = 4.0
+        # set up temp label
+        self.lbl_temp = self.config.ui.get_object("lbl-temp")
+        self.ok_range = 4.0
 
-  def update_temperatures(self, temp):
-    time = temp['time']
-    for tool in self.temps:
-      if tool in temp:
-        t = temp[tool]["actual"]
-        plot = self.temps[tool]["g_actual"]
-        plot.add_point(time, t)
-        t = temp[tool]["target"]
-        plot = self.temps[tool]["g_target"]
-        plot.add_point(time, t)
-    self.graph.refresh()
+    def update_temperatures(self, temp):
+        time = temp['time']
+        for tool in self.temps:
+            if tool in temp:
+                t = temp[tool]["actual"]
+                plot = self.temps[tool]["g_actual"]
+                plot.add_point(time, t)
+                t = temp[tool]["target"]
+                plot = self.temps[tool]["g_target"]
+                plot.add_point(time, t)
+        self.graph.refresh()
 
-  def update_temperature_status(self, temp):
-    for tool in self.temps:
-      if tool in temp:
-        self.temps[tool]["t_actual"] = temp[tool]["actual"]
-        self.temps[tool]["t_target"] = temp[tool]["target"]
-    if "tool1" in temp:
-      self.lbl_temp.set_text("B:{} T0:{} T1:{}".format(
-          temp["bed"]["actual"], temp["tool0"]["actual"], temp["tool1"]["actual"]))
-    else:
-      self.lbl_temp.set_text("B:{} T0:{}".format(temp["bed"]["actual"], temp["tool0"]["actual"]))
+    def update_temperature_status(self, temp):
+        for tool in self.temps:
+            if tool in temp:
+                self.temps[tool]["t_actual"] = temp[tool]["actual"]
+                self.temps[tool]["t_target"] = temp[tool]["target"]
 
-    # Update preheat button states
-    self.update_states()
+        self.lbl_temp.set_text("B:{} T0:{} T1:{}".format(temp["bed"]["actual"], temp["tool0"]["actual"],
+                                                         temp["tool1"]["actual"]))
 
-  # Retrieve the current state of the heating
-  # elements
-  def update_states(self):
-    for tool in self.temps:
-      if self.temps[tool]["t_target"] > 0:
-        self.temps[tool]["heating"] = True
-        diff = self.temps[tool]["t_target"] - self.temps[tool]["t_actual"]
-        if abs(diff) < self.ok_range:
-          self.temps[tool]["state"] = "heated"
-          if tool == "bed":
-            self.temps[tool]["btn"].set_style_class("hot_bed")
-          else:
-            self.temps[tool]["btn"].set_style_class("hot")
-        elif diff > 0:
-          self.temps[tool]["state"] = "heating"
-          if tool == "bed":
-            self.temps[tool]["btn"].set_style_class("heating_bed")
-          else:
-            self.temps[tool]["btn"].set_style_class("heating")
+        # Update preheat button states
+        self.update_states()
+
+    # Retrieve the current state of the heating
+    # elements
+    def update_states(self):
+        for tool in self.temps:
+            if self.temps[tool]["t_target"] > 0:
+                self.temps[tool]["heating"] = True
+                diff = self.temps[tool]["t_target"] - \
+                    self.temps[tool]["t_actual"]
+                if abs(diff) < self.ok_range:
+                    self.temps[tool]["state"] = "heated"
+                    if tool == "bed":
+                        self.temps[tool]["btn"].set_style_class("hot_bed")
+                    else:
+                        self.temps[tool]["btn"].set_style_class("hot")
+                elif diff > 0:
+                    self.temps[tool]["state"] = "heating"
+                    if tool == "bed":
+                        self.temps[tool]["btn"].set_style_class("heating_bed")
+                    else:
+                        self.temps[tool]["btn"].set_style_class("heating")
+                else:
+                    self.temps[tool]["state"] = "cooling"
+            else:
+                if tool == "bed":
+                    self.temps[tool]["btn"].set_style_class("cold_bed")
+                else:
+                    self.temps[tool]["btn"].set_style_class("cold")
+
+    def on_preheat_tool0(self, button, action):
+        if self.temps["tool0"]["heating"]:
+            self.config.rest_client.set_tool_temp(0, 0)
+            self.temps["tool0"]["btn"].set_style_class("cold")
+            self.temps["tool0"]["heating"] = False
         else:
-          self.temps[tool]["state"] = "cooling"
-      else:
-        if tool == "bed":
-          self.temps[tool]["btn"].set_style_class("cold_bed")
+            new_temp = self.temps["tool0"]["t_preheat"]
+            self.config.rest_client.set_tool_temp(0, new_temp)
+            self.temps["tool0"]["btn"].set_style_class("heating")
+            self.temps["tool0"]["heating"] = True
+
+    def on_preheat_tool1(self, button, action):
+        if self.temps["tool1"]["heating"]:
+            self.config.rest_client.set_tool_temp(1, 0)
+            self.temps["tool1"]["btn"].set_style_class("cold")
+            self.temps["tool1"]["heating"] = False
         else:
-          self.temps[tool]["btn"].set_style_class("cold")
+            new_temp = self.temps["tool1"]["t_preheat"]
+            self.config.rest_client.set_tool_temp(1, new_temp)
+            self.temps["tool1"]["btn"].set_style_class("heating")
+            self.temps["tool1"]["heating"] = True
 
-  def on_preheat_tool0(self, button, action):
-    if self.temps["tool0"]["heating"]:
-      self.config.rest_client.set_tool_temp(0, 0)
-      self.temps["tool0"]["btn"].set_style_class("cold")
-      self.temps["tool0"]["heating"] = False
-    else:
-      new_temp = self.temps["tool0"]["t_preheat"]
-      self.config.rest_client.set_tool_temp(0, new_temp)
-      self.temps["tool0"]["btn"].set_style_class("heating")
-      self.temps["tool0"]["heating"] = True
+    def on_preheat_tool2(self, button, action):
+        if self.temps["tool2"]["heating"]:
+            self.config.rest_client.set_tool_temp(2, 0)
+            self.temps["tool2"]["btn"].set_style_class("cold")
+            self.temps["tool2"]["heating"] = False
+        else:
+            new_temp = self.temps["tool2"]["t_preheat"]
+            self.config.rest_client.set_tool_temp(2, new_temp)
+            self.temps["tool2"]["btn"].set_style_class("heating")
+            self.temps["tool2"]["heating"] = True
 
-  def on_preheat_tool1(self, button, action):
-    if self.temps["tool1"]["heating"]:
-      self.config.rest_client.set_tool_temp(1, 0)
-      self.temps["tool1"]["btn"].set_style_class("cold")
-      self.temps["tool1"]["heating"] = False
-    else:
-      new_temp = self.temps["tool1"]["t_preheat"]
-      self.config.rest_client.set_tool_temp(1, new_temp)
-      self.temps["tool1"]["btn"].set_style_class("heating")
-      self.temps["tool1"]["heating"] = True
+    def on_preheat_bed(self, button, action):
+        if self.temps["bed"]["heating"]:
+            self.config.rest_client.set_bed_temp(0)
+            self.temps["bed"]["btn"].set_style_class("cold_bed")
+            self.temps["bed"]["heating"] = False
+        else:
+            new_temp = self.temps["bed"]["t_preheat"]
+            self.config.rest_client.set_bed_temp(new_temp)
+            self.temps["bed"]["btn"].set_style_class("heating_bed")
+            self.temps["bed"]["heating"] = True
 
-  def on_preheat_tool2(self, button, action):
-    if self.temps["tool2"]["heating"]:
-      self.config.rest_client.set_tool_temp(2, 0)
-      self.temps["tool2"]["btn"].set_style_class("cold")
-      self.temps["tool2"]["heating"] = False
-    else:
-      new_temp = self.temps["tool2"]["t_preheat"]
-      self.config.rest_client.set_tool_temp(2, new_temp)
-      self.temps["tool2"]["btn"].set_style_class("heating")
-      self.temps["tool2"]["heating"] = True
-
-  def on_preheat_bed(self, button, action):
-    if self.temps["bed"]["heating"]:
-      self.config.rest_client.set_bed_temp(0)
-      self.temps["bed"]["btn"].set_style_class("cold_bed")
-      self.temps["bed"]["heating"] = False
-    else:
-      new_temp = self.temps["bed"]["t_preheat"]
-      self.config.rest_client.set_bed_temp(new_temp)
-      self.temps["bed"]["btn"].set_style_class("heating_bed")
-      self.temps["bed"]["heating"] = True
-
-  def change_to_filament(self, button, action):
-    if self.config.getboolean('System', 'use-filament-graph'):
-      self.graph.hide()
-      self.config.filament_graph.graph.show()
-      #self.config.filament_graph.graph.refresh()
+    def change_to_filament(self, button, action):
+        if self.config.getboolean('System', 'use-filament-graph'):
+            self.graph.hide()
+            self.config.filament_graph.graph.show()
+            # self.config.filament_graph.graph.refresh()
--- toggle/WebSocksClient.py
+++ toggle/WebSocksClient.py
@@ -26,168 +26,181 @@ DEFAULT_REQUEST_TIMEOUT = 60
 
 
 class WebSocksClient():
-  """
-  Base for web socket clients.
-  """
-
-  CLOSED = 0
-  CONNECTING = 1
-  CONNECTED = 2
-  FAILED = 3
-
-  def __init__(self, config=None, host="ws://kamikaze.local"):
-    self.config = config
-    self.host = host
-    self.connect_timeout = 60
-    self.request_timeout = 60
-    self._prefix = "/sockjs"
-    self._r1 = str(random.randint(0, 1000))
-    self._conn_id = self.random_str(8)
-
-    self.url = host + '/'.join([self._prefix, 'websocket'])
-
-    self.max_reconnects = 10
-    self.state = WebSocksClient.CLOSED
-    self.io_loop = ioloop.IOLoop.current()
-
-  def connect(self):
-    logging.debug("Connecting to " + self.url)
-    self.state = WebSocksClient.CONNECTING
-    self._ws_connection = websocket.websocket_connect(
-        self.url, io_loop=self.io_loop, callback=self._connect_callback)
-
-  def authenticate(self, apikey):
-    user = self.config.get("WebSocket", "user")
-    logging.debug("Authenticating with " + user + ":" + apikey)
-    msg = '{ "auth" : "' + str(user) + ':' + str(apikey) + '" }'
-    logging.info("Sending message " + msg)
-    self._ws_connection.write_message(msg)
-
-  def send(self, data):
     """
-    Send message to the server
-      :param str data: message.
-    """
-    logging.info("Sending message " + data)
-    yield self._ws_connection.write_message(data)
-
-  def close_conn(self):
-    """
-    Close connection.
+    Base for web socket clients.
     """
 
-    if not self._ws_connection:
-      raise RuntimeError('Web socket connection is already closed.')
+    CLOSED = 0
+    CONNECTING = 1
+    CONNECTED = 2
+    FAILED = 3
 
-    self._ws_connection.close()
+    def __init__(self, config=None, host="ws://kamikaze.local"):
+        self.config = config
+        self.host = host
+        self.connect_timeout = 60
+        self.request_timeout = 60
+        self._prefix = "/sockjs"
+        self._r1 = str(random.randint(0, 1000))
+        self._conn_id = self.random_str(8)
 
-  def _connect_callback(self, future):
-    if future.exception() is None:
-      self._ws_connection = future.result()
-      self._on_connection_success()
-      self._read_messages()
-    else:
-      self._on_connection_error(future.exception())
+        self.url = host + \
+            '/'.join([self._prefix, self._r1, self._conn_id, 'websocket'])
 
-  @gen.coroutine
-  def _read_messages(self):
-    while True:
-      msg = yield self._ws_connection.read_message()
-      if msg is None:
-        self._on_connection_close()
-        break
+        self.max_reconnects = 10
+        self.state = WebSocksClient.CLOSED
 
-      self._on_message(msg)
+        self.io_loop = ioloop.IOLoop.current()
 
-  def _on_message(self, msg):
-    """
-    This is called when new message is available from the server.
-      :param str msg: server message.
-    """
-    data = json.loads(msg)
-    if 'connected' in data:
-      logging.debug("SockJS: Socket connected")
-      apik = data['connected']['apikey']
-      self.authenticate(apik)
+    def connect(self):
+        logging.debug("Connecting to " + self.url)
+        self.state = WebSocksClient.CONNECTING
+        self.conn = websocket.websocket_connect(
+            self.url, io_loop=self.io_loop, callback=self._connect_callback)
 
-    self.parse_msg(data)
+    def send(self, data):
+        """
+        Send message to the server
+          :param str data: message.
+        """
+        if not self._ws_connection:
+            raise RuntimeError('Web socket connection is closed.')
 
-  def _on_connection_success(self):
-    """
-    This is called on successful connection ot the server.
-    """
-    self.state = WebSocksClient.CONNECTED
-    logging.debug('Websocket connected!')
+        self._ws_connection.write_message(escape.utf8(json.dumps(data)))
 
-  def _on_connection_close(self):
-    """
-    This is called when server closed the connection.
-    """
-    self.state = WebSocksClient.CLOSED
-    logging.debug('Websocket connection closed!')
+    def close_conn(self):
+        """
+        Close connection.
+        """
 
-  def _on_connection_error(self, exception):
-    """
-    This is called in case if connection to the server could
-    not established.
-    """
-    self.state = WebSocksClient.FAILED
-    self.io_loop.stop()
-    logging.debug('Websocket connection error: %s', exception)
+        if not self._ws_connection:
+            raise RuntimeError('Web socket connection is already closed.')
 
-  def parse_msg(self, data):
-    try:
-      msg_type, payload = data.popitem()
-      p = PushUpdate(msg_type, payload)
-      self.config.push_updates.put(p)
-    except Exception as e:
-      logging.warning("Unable to parse message from Octoprint " + str(e))
-      logging.warning("messsage was " + str(msg))
+        self._ws_connection.close()
 
-  def random_str(self, length):
-    letters = string.ascii_lowercase + string.digits
-    return ''.join(random.choice(letters) for c in range(length))
+    def _connect_callback(self, future):
+        if future.exception() is None:
+            self._ws_connection = future.result()
+            self._on_connection_success()
+            self._read_messages()
+        else:
+            self._on_connection_error(future.exception())
 
-  def run(self):
-    for i in range(self.config.getint("Rest", "timeout")):
-      if self.running:
-        self.config.splash.set_status("Connecting to {} ({})".format(self.host, i))
-        logging.debug("Websocket connection attempt " + str(i))
-        self.connect()
-        self.io_loop.start()
-        time.sleep(1)
-    self.config.splash.set_status("Unable to connect to " + self.host)
-    self.config.splash.enable_next()
+    @gen.coroutine
+    def _read_messages(self):
+        while True:
+            msg = yield self._ws_connection.read_message()
+            if msg is None:
+                self._on_connection_close()
+                break
 
-  def start(self):
-    self.running = True
-    self.thread = threading.Thread(target=self.run)
-    self.thread.start()
+            self._on_message(msg)
 
-  def stop(self):
-    self.running = False
-    self.io_loop.stop()
-    self.thread.join()
+    def _on_message(self, msg):
+        """
+        This is called when new message is available from the server.
+          :param str msg: server message.
+        """
+        # print(msg)
+        data = msg[0]
+        payload = msg[1:]
+        if data == 'o':
+            logging.debug("SockJS: Socket connected")
+        elif data == 'c':
+            logging.debug(
+                "SockJS: Socket disconnected, reason: " +
+                str(payload))
+        elif data == 'h':
+            self.config.printer.flash_heartbeat()
+            #logging.debug("SockJS: Heartbeat "+str(msg))
+        elif data in ['a', 'm']:
+            if self.config:
+                self.parse_msg(payload)
+            else:
+                logging.debug(payload)
+        else:
+            logging.debug("Got garbled char " + str(msg) +
+                          " hex: " + str(int(msg, 16)))
+
+    def _on_connection_success(self):
+        """
+        This is called on successful connection ot the server.
+        """
+        self.state = WebSocksClient.CONNECTED
+        logging.debug('Webscket connected!')
+
+    def _on_connection_close(self):
+        """
+        This is called when server closed the connection.
+        """
+        self.state = WebSocksClient.CLOSED
+        logging.debug('Websocket connection closed!')
+
+    def _on_connection_error(self, exception):
+        """
+        This is called in case if connection to the server could
+        not established.
+        """
+        self.state = WebSocksClient.FAILED
+        self.io_loop.stop()
+        logging.debug('Websocket connection error: %s', exception)
+
+    def parse_msg(self, msg):
+        try:
+            data = json.loads(msg)[0]
+            msg_type, payload = data.popitem()
+            p = PushUpdate(msg_type, payload)
+            self.config.push_updates.put(p)
+        except Exception as e:
+            logging.warning("Unable to parse message from Octoprint " + str(e))
+            logging.warning("messsage was " + str(msg))
+
+    def random_str(self, length):
+        letters = string.ascii_lowercase + string.digits
+        return ''.join(random.choice(letters) for c in range(length))
+
+    def run(self):
+        for i in range(self.config.getint("Rest", "timeout")):
+            if self.running:
+                self.config.splash.set_status(
+                    "Connecting to {} ({})".format(self.host, i))
+                logging.debug("Websocket connection attempt " + str(i))
+                self.connect()
+                self.io_loop.start()
+                time.sleep(1)
+        self.config.splash.set_status("Unable to connect to " + self.host)
+        self.config.splash.enable_next()
+
+    def start(self):
+        self.running = True
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
+
+    def stop(self):
+        self.running = False
+        self.io_loop.stop()
+        self.thread.join()
 
 
 def main():
-  logging.basicConfig(
-      level=logging.DEBUG,
-      format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-      datefmt='%m-%d %H:%M')
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M')
 
-  client = WebSocksClient(None, "ws://kamikaze.local")
+    #client = WebSocksClient(None, "ws://kamikaze.local")
+    client = WebSocksClient(None, "ws://localhost")
 
-  def work():
-    for i in range(10):
-      print(i)
-      time.sleep(1)
+    def work():
+        for i in range(10):
+            print (i)
+            time.sleep(1)
 
-  client.start()
-  threading.Thread(target=work).start()
-  time.sleep(10)
-  client.stop()
+    client.start()
+    threading.Thread(target=work).start()
+    time.sleep(10)
+    client.stop()
 
 
 if __name__ == '__main__':
-  main()
+    main()
