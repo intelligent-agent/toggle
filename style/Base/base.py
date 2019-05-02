3f006b17a6a811746153e6fb8c122e57106e65b6
Ben Glick <glick@lclark.edu>
2019-05-02 13:22:53 -0700

    Adding compositional ui docs

--- style/Base/base.py
+++ style/Base/base.py
@@ -6,6 +6,34 @@ import clutter_children
 import base_color_scheme
 import wifi_children
 
+
+"""
+This is the start of our compositional UI config system.
+
+We've broken out the large JSON file into several small python files,
+each of which contain dictionaries which dynamically generate UI config
+JSONs, based on the current screen resolution and the base color scheme,
+which can now be changed more simply.
+
+The idea of this is that it will be run at runtime, and generate a JSON config
+which is backwards-compatible at every run. Based on this, users will be able to 
+change screens and other options without manually editing large and complex json files.
+
+We've broken this compositional UI config system into several pieces:
+
+  - base.py, the general structure and scripts to actually generate config files
+
+  - base_color_scheme.py, which stores information about the color scheme. Users
+                          who wish to change the colors should be able to edit only
+                          that file, which is quite simple.
+
+  - box_children.py, which stores information about what boxes are in the Toggle UI
+
+  - side<N>.py where N={0,1,2,3}, stores information about
+
+  - wifi_children.py, which 
+"""
+
 ### finish imports ###
 bc = box_children.box_children
 cc = clutter_children.clutter_children
