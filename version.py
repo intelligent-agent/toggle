6c2d1eaa50587132a7c62d62a86d1e569e1ab8f8
Elias Bakken <elias@iagent.no>
2019-07-17 15:45:55 +0200

    Non finished version script

---
+++ version.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+from pkg_resources import parse_version
+from toggle.__init__ import __version__
+
+def get_local_version():
+    return __version__
+
+def get_remote_version():
+    return "v1.0.0"
+
+def get_version():
+    local_version = get_local_version()
+    remote_version = get_remote_version()
+    # Print local, remote and return code as pr OctoPrints request.
+    print(local_version)
+    print(remote_version)
+    if parse_version(remote_version) > parse_version(local_version):
+        sys.exit(0)
+    sys.exit(1)
+
+if __name__ == '__main__':
+  get_version()
