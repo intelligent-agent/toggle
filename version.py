a86b2b49d1975e3634285e65faf2b3b416d26e0d
Elias Bakken <elias@iagent.no>
2019-07-17 16:05:59 +0200

    A version script that looks good

--- version.py
+++ version.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import sys
+import requests
 from pkg_resources import parse_version
 from toggle.__init__ import __version__
+
+URL = "https://api.github.com/repos/intelligent-agent/toggle/releases/latest"
 
 def get_local_version():
     return __version__
 
 def get_remote_version():
-    return "v1.0.0"
+    r = requests.get(URL)
+    return r.json()["tag_name"]
 
 def get_version():
     local_version = get_local_version()
