9f95e4f95b39047e8c466c9f5b1164c310b633e8
Elias Bakken <elias@iagent.no>
2019-07-20 17:56:06 +0200

    Fixed formatting

--- tests/test_CascadingConfigParser.py
+++ tests/test_CascadingConfigParser.py
@@ -2,9 +2,10 @@ import pytest
 from os import listdir, getcwd
 from os.path import isfile, join, basename, dirname, abspath
 
+
 def test_all_config_files(default_config):
-    default = default_config
-    path = default.config_location
-    config_files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
-    for f in config_files:
-        assert(default.check_file_valid(f))
+  default = default_config
+  path = default.config_location
+  config_files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+  for f in config_files:
+    assert (default.check_file_valid(f))
