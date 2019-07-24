8008c84636c25af8fd72c269be142d50fab9696a
Elias Bakken <elias@iagent.no>
2019-07-24 23:46:45 +0200

    Splitting test and dropping test of networkamanger

--- tests/conformance/test_network_manager.py
+++ tests/conformance/test_network_manager.py
@@ -1,15 +1,8 @@
 import pytest
 
-pytest.importorskip("pyconnman")
 pytest.importorskip("NetworkManager")
-from toggle.Network import ConnMan, NetworkManager
+from toggle.Network import NetworkManager
 from dbus.mainloop.glib import DBusGMainLoop
-
-
-def test_connman(default_config):
-  DBusGMainLoop(set_as_default=True)
-  cm = ConnMan(default_config)
-  assert (1)
 
 
 def test_network_manager(default_config):
