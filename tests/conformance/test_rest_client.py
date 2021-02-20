4feaa897d3526b05dafbfe5afdf2f3edbdb9c142
Elias Bakken <elias@iagent.no>
2021-02-20 16:56:18 +0100

    Adding tests to rest client, adding method for getting list of slicers (#76)

--- tests/conformance/test_rest_client.py
+++ tests/conformance/test_rest_client.py
@@ -6,6 +6,12 @@ import json
 from toggle.core.RestClient import RestClient
 
 
+@requests_mock.Mocker(kw='mock')
+def test_connection_ok(default_config, **kwargs):
+  kwargs['mock'].get('http://localhost:5000/api/version')
+  assert RestClient(default_config).connection_ok() == True
+
+
 def test_get_list_of_files(default_config):
   with requests_mock.Mocker(real_http=True) as m:
     m.get('http://localhost:5000/api/files', json={})
@@ -32,3 +38,102 @@ def test_login_status_code_403(default_config):
     m.post('http://localhost:5000/api/login', status_code=403)
     client = RestClient(default_config)
     assert client.login() == "INVALID-SESSION"
+
+
+def test_start_job(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/job', status_code=204)
+    client = RestClient(default_config)
+    assert client.start_job()
+
+
+def test_pause_job(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/job', status_code=204)
+    client = RestClient(default_config)
+    assert client.pause_job()
+
+
+def test_cancel_job(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/job', status_code=204)
+    client = RestClient(default_config)
+    assert client.cancel_job()
+
+
+def test_resume_job(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/job', status_code=204)
+    client = RestClient(default_config)
+    assert client.resume_job()
+
+
+def test_send_gcode(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/printer/command', status_code=204)
+    client = RestClient(default_config)
+    assert client.send_gcode("G0 X0")
+
+
+def test_set_bed_temp(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/printer/bed', status_code=204)
+    client = RestClient(default_config)
+    assert client.set_bed_temp(42)
+
+
+def test_set_tool_temp(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/printer/tool', status_code=204)
+    client = RestClient(default_config)
+    assert client.set_tool_temp(0, 42)
+
+
+def test_select_file(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/files/local/Dolf-Lundgren', status_code=204)
+    client = RestClient(default_config)
+    assert client.select_file("Dolf-Lundgren")
+
+
+def test_jog(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/printer/printhead', status_code=204)
+    client = RestClient(default_config)
+    assert client.jog({"X": 42})
+    client.jog({"X": 42})
+
+
+def test_home(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/printer/printhead', status_code=204)
+    client = RestClient(default_config)
+    assert client.home("X")
+
+
+def test_extrude(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/printer/tool', status_code=204)
+    client = RestClient(default_config)
+    assert client.extrude(7)
+
+
+def test_select_tool(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/printer/tool', status_code=204)
+    client = RestClient(default_config)
+    assert client.select_tool(0)
+
+
+def test_download_model(default_config):
+  with requests_mock.Mocker() as m:
+    m.get("http://chuck-norris.com", content=b"Bruce-Willis")
+    client = RestClient(default_config)
+    assert client.download_model("http://chuck-norris.com") == b"Bruce-Willis"
+
+
+def test_get_slicers(default_config):
+  with requests_mock.Mocker(real_http=True) as m:
+    m.post('http://localhost:5000/api/slicing', json={"arnhold-slicer": {}})
+    client = RestClient(default_config)
+    assert client.get_slicers()["arnhold-slicer"] == {}
--- toggle/core/RestClient.py
+++ toggle/core/RestClient.py
@@ -3,7 +3,6 @@ import requests
 import json
 
 import logging
-""" Rest API Client class  """
 
 
 class RestClient:
@@ -19,7 +18,7 @@ class RestClient:
     self._headers = {'Content-Type': 'application/json', 'X-Api-Key': self._api_key}
 
   def login(self):
-    url = "http://" + self._host + ":" + str(self._port) + "/api/login"
+    url = self._build_url("login")
     user = self.config.get("OctoPrint", "user")
     password = self.config.get("OctoPrint", "password")
     data = json.dumps({'user': user, 'pass': password})
@@ -31,48 +30,36 @@ class RestClient:
       return "INVALID-SESSION"
 
   def connection_ok(self):
-    r = requests.get(self._build_url("/api/version"), headers=self._headers)
+    r = requests.get(self._build_url("version"), headers=self._headers)
     return r.status_code == 200
 
   def start_job(self):
-    logging.debug("Starting job")
-    url = "http://" + self._host + ":" + str(self._port) + "/api/job"
     data = json.dumps({'command': 'start'})
-    r = requests.post(url, data=data, headers=self._headers)
-    print(r.json)
+    r = requests.post(self._build_url("job"), data=data, headers=self._headers)
+    return r.status_code == 204
 
   def pause_job(self):
-    logging.debug("Pausing job")
-    url = "http://" + self._host + ":" + str(self._port) + "/api/job"
     data = json.dumps({'command': 'pause'})
-    r = requests.post(url, data=data, headers=self._headers)
-    print(r.json)
+    r = requests.post(self._build_url("job"), data=data, headers=self._headers)
+    return r.status_code == 204
 
   def cancel_job(self):
-    logging.debug("Cancelling job")
-    url = "http://" + self._host + ":" + str(self._port) + "/api/job"
     data = json.dumps({'command': 'cancel'})
-    r = requests.post(url, data=data, headers=self._headers)
-    print(r.json)
+    r = requests.post(self._build_url("job"), data=data, headers=self._headers)
+    return r.status_code == 204
 
   def resume_job(self):
-    logging.debug("Resuming job")
-    url = "http://" + self._host + ":" + str(self._port) + "/api/job"
     data = json.dumps({'command': 'pause'})
-    r = requests.post(url, data=data, headers=self._headers)
-    print(r.json)
+    r = requests.post(self._build_url("job"), data=data, headers=self._headers)
+    return r.status_code == 204
 
   def send_gcode(self, cmd):
-    logging.debug("Sending gcode")
-    url = "http://" + self._host + ":" + \
-        str(self._port) + "/api/printer/command"
+    url = self._build_url("printer/command")
     data = json.dumps({'command': cmd})
     r = requests.post(url, data=data, headers=self._headers)
-    if r.status_code != 204:
-      logging.warning(f"send_gcode returned satus code: {r.status_code}")
+    return r.status_code == 204
 
   def start_preheat(self):
-    logging.debug("Starting preheat")
     bed_temp = self.config.get("Preheat", "bed_temp")
     tool_0 = self.config.get("Preheat", "t0_temp")
     tool_1 = self.config.get("Preheat", "t1_temp")
@@ -81,65 +68,53 @@ class RestClient:
     self.set_tool_temp(1, tool_1)
 
   def stop_preheat(self):
-    logging.debug("Stopping preheat")
     self.set_bed_temp(0)
     self.set_tool_temp(0, 0)
     self.set_tool_temp(1, 0)
 
   def set_bed_temp(self, temp):
-    url = "http://" + self._host + ":" + \
-        str(self._port) + "/api/printer/bed"
+    url = self._build_url("printer/bed")
     data = json.dumps({'command': 'target', 'target': int(float(temp))})
     r = requests.post(url, data=data, headers=self._headers)
+    return r.status_code == 204
 
   def set_tool_temp(self, tool_nr, temp):
-    url = "http://" + self._host + ":" + \
-        str(self._port) + "/api/printer/tool"
+    url = self._build_url("printer/tool")
     data = json.dumps({'command': 'target', 'targets': {'tool' + str(tool_nr): int(float(temp))}})
     r = requests.post(url, data=data, headers=self._headers)
+    return r.status_code == 204
 
   def select_file(self, filename):
-    url = "http://" + self._host + ":" + \
-        str(self._port) + "/api/files/local/" + filename
+    url = self._build_url("files/local/" + filename)
     data = json.dumps({'command': 'select'})
     r = requests.post(url, data=data, headers=self._headers)
     return r.status_code == 204
 
   # Jog the printer
   def jog(self, amount):
-    url = "http://" + self._host + ":" + \
-        str(self._port) + "/api/printer/printhead"
-    data = json.dumps({
-        'command': 'jog',
-        'x': amount["x"] if "x" in amount else 0,
-        'y': amount["y"] if "y" in amount else 0,
-        'z': amount["z"] if "z" in amount else 0
-    })
-    r = requests.post(url, data=data, headers=self._headers)
+    data = json.dumps({'command': 'jog', **amount})
+    r = requests.post(self._build_url("printer/printhead"), data=data, headers=self._headers)
+    return r.status_code == 204
 
   # Home selected axes
   def home(self, axes):
-    url = "http://" + self._host + ":" + \
-        str(self._port) + "/api/printer/printhead"
     data = json.dumps({'command': 'home', 'axes': axes})
-    r = requests.post(url, data=data, headers=self._headers)
+    r = requests.post(self._build_url("printer/printhead"), data=data, headers=self._headers)
+    return r.status_code == 204
 
-  # Extrude
   def extrude(self, amount):
-    url = "http://" + self._host + ":" + \
-        str(self._port) + "/api/printer/tool"
     data = json.dumps({'command': 'extrude', 'amount': amount})
-    r = requests.post(url, data=data, headers=self._headers)
+    r = requests.post(self._build_url("printer/tool"), data=data, headers=self._headers)
+    return r.status_code == 204
 
-  # Select Extruder E/H
   def select_tool(self, tool):
     data = json.dumps({'command': 'select', 'tool': tool})
-    r = requests.post(self._build_url("/api/printer/tool"), data=data, headers=self._headers)
+    r = requests.post(self._build_url("printer/tool"), data=data, headers=self._headers)
     return r.status_code in [200, 204]
 
   def get_list_of_files(self):
     try:
-      r = requests.get(self._build_url("/api/files"), headers=self._headers)
+      r = requests.get(self._build_url("files"), headers=self._headers)
     except requests.ConnectionError as e:
       logging.warning("Connection error")
       return {'files': []}
@@ -160,5 +135,12 @@ class RestClient:
     logging.warning("Unable to download file. Got response: " + r.status_code)
     return None
 
+  def get_slicers(self):
+    r = requests.post(self._build_url("slicing"), headers=self._headers)
+    if r.status_code == 200:
+      return r.json()
+    logging.warning("Unable to gt slicers: " + r.status_code)
+    return {}
+
   def _build_url(self, path):
-    return "http://" + self._host + ":" + str(self._port) + path
+    return f"http://{self._host}:{self._port}/api/{path}"
