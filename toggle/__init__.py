a7fc8394502506b6d0668639f5d3ceff7990b687
Elias Bakken <elias@iagent.no>
2026-02-19 20:55:37 +0100

    Upgrade versioneer

--- toggle/__init__.py
+++ toggle/__init__.py
@@ -24,6 +24,5 @@ except ValueError:
 
 del get_versions
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
